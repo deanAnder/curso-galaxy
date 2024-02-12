6fbf7583477f2d157e5d304a98008ceed74806a9
deanAnder <dean2anderhm@gmail.com>
2024-02-12 18:57:42 -0500

    registry-server

---
+++ ms-administracion-server-registry/src/main/java/pe/edu/galaxy/training/java/ms/solucion/pedidos/msadministracionserverregistry/MsAdministracionServerRegistryApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.galaxy.training.java.ms.solucion.pedidos.msadministracionserverregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdministracionServerRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdministracionServerRegistryApplication.class, args);
+	}
+
+}
